96e0e4a146a8b7cabf234d976d97900843693d43
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-12-12 09:53:45 -0800

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 from openai import OpenAI
 
-def load_api_key(file_path='API_keys.json'):
+def load_api_key(file_path='API_keys.json') -> str:
+    # load API key from JSON file
     try:
         with open(file_path, 'r') as file:
             keys = json.load(file)
@@ -13,7 +14,8 @@ def load_api_key(file_path='API_keys.json'):
         print("Error reading the API_keys.json file.")
         return ''
     
-def load_character_descriptions(file_path='character_descriptions.json'):
+def load_character_descriptions(file_path='character_descriptions.json') -> (dict, str):
+    # load character descriptions from JSON file
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
@@ -33,23 +35,29 @@ def load_character_descriptions(file_path='character_descriptions.json'):
         print(f"Error reading the {file_path} file.")
         return {}, ""
 
-def select_character(characters):
+def select_character(characters: list) -> str:
+    # let the user choose a character
+    
+    # print a character
     print("Choose the character to have a conversation with:")
     for character in characters:
         print(f"- {character}")
     
+    # repeat until the user chooses a valid character from the list
     selected_character = ""
     while selected_character not in characters:
         selected_character = input("Your choice: ")
         if selected_character not in characters:
             print("Character not found. Please choose from the list.")
 
+    # print the selected character's current emotion
     current_character_object = characters[selected_character]
     print(f"Character's current emotion: {current_character_object.emotion}\n")
     
     return selected_character
 
 class FSM:
+    # our pre-defined FSM class
     def __init__(self, initial_state):
         # Dictionary (input_symbol, current_state) --> (action, next_state).
         self.state_transitions = {}
@@ -108,7 +116,7 @@ class FSM:
         if next_state: self.current_state = next_state
 
 class Character:
-    def __init__(self, name, description, emotion):
+    def __init__(self, name, description, emotion) -> None:
         self.name = name
         self.description = description
         self.emotion = emotion
@@ -120,37 +128,50 @@ class Character:
 
         # emotion_types = ["neutral", "happy", "excited", "stressed"]
 
-    def add_to_conversation(self, role, content):
+    def add_to_conversation(self, role, content) -> None:
+        # store message in LLM's conversation history
         self.conversation_history.append({"role": role, "content": content})
 
     def get_conversation_history(self):
+        # get past messages
         return self.conversation_history
     
-    def update_emotion_based_on_conversation(self, conversation_type):
+    def update_emotion_based_on_conversation(self, conversation_type) -> None:
+        # adjust the LLM's emotion based on the conversation type they assessed
         self.fsm.process(conversation_type)
         self.emotion = self.fsm.current_state
 
-    def setup_fsm_transitions(self):
+    def setup_fsm_transitions(self) -> None:
         # Example transitions (add more as needed)
         # states: happy, stressed, neutral
         # transitions: fun, stressful, boring
         # the FSM for this would be triangular -- each of the three emotions can map to any other emotion
+
+        # transitions for fun conversations
         self.fsm.add_transition("fun", "happy", next_state="happy")
         self.fsm.add_transition("fun", "neutral", next_state="happy")
         self.fsm.add_transition("fun", "stressed", next_state="happy")
 
+        # transitions for boring conversations
         self.fsm.add_transition("boring", "neutral", next_state="neutral")
         self.fsm.add_transition("boring", "happy", next_state="neutral")
         self.fsm.add_transition("boring", "stressed", next_state="neutral")
         
+        # transitions for stressful conversations
         self.fsm.add_transition("stressful", "stressed", next_state="stressed")
         self.fsm.add_transition("stressful", "happy", next_state="stressed")
         self.fsm.add_transition("stressful", "neutral", next_state="stressed")
 
-def chat_with_gpt3(character, user_message, model="gpt-4-1106-preview"):
+def chat_with_gpt3(character: Character, user_message: str, model="gpt-4-1106-preview") -> str:
+    # function to get response from LLM based on user input
     try:
+        # restart the OpenAI Client
         client = OpenAI(api_key=load_api_key())
+
+        # give the model its past conversation history with the user
         conversation = character.get_conversation_history() + [{"role": "user", "content": user_message}]
+
+        # get the response from the LLM
         response = client.chat.completions.create(
             model=model,
             messages=conversation
@@ -159,8 +180,8 @@ def chat_with_gpt3(character, user_message, model="gpt-4-1106-preview"):
     except Exception as e:
         return str(e)
 
-def main():
-    # Create character instances
+def main() -> None:
+    # load pre-defined character descriptions from file
     character_descriptions, evaluate_conversation_prompt = load_character_descriptions()
 
     # Create character instances with descriptions and starting emotions
@@ -169,20 +190,27 @@ def main():
         for name, char_data in character_descriptions.items()
     }
 
+    # let the user pick a character
     current_character_name = select_character(characters)
     current_character = characters[current_character_name]
 
+    # start the conversation
     print(f"Starting conversation with {current_character_name}. Type 'quit' to exit or 'switch' to change character.")
 
+    # repeat until the game ends
     while True:
+        # get the user's input, process their response
         user_input = input("You: ")
+
         if user_input.lower() == 'quit':
             break
         elif user_input.lower() == 'switch':
+            # if the user switches the character assess the conversation, update the LLM's emotion
             conversation_assessment = chat_with_gpt3(current_character, user_message=evaluate_conversation_prompt)
             current_character.update_emotion_based_on_conversation(conversation_assessment)
             print(f"Conversation Type: {conversation_assessment} \n")
 
+            # let the user pick a new character
             current_character_name = select_character(characters)
             current_character = characters[current_character_name]
             continue
